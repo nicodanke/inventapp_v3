8ce29491c12a8c7178a802f2ded12a81bb714346
Nicolas Dankiewicz <nicodanki@hotmail.com>
2024-12-29 19:23:48 -0300

    feat: remove V2 code

--- account-service/vendor/github.com/nicodanke/inventapp_v3/account-service/token/maker.go
+++
@@ -1,9 +0,0 @@
-package token
-
-import "time"
-
-type Maker interface {
-	CreateToken(userId int64, accountId int64, accountCode string, duration time.Duration) (string, *Payload, error)
-
-	VerifyToken(token string) (*Payload, error)
-}
